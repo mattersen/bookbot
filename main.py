c89592efd8ac189acc2a3ff05a8cc5021f0cf851
mattersen <mattersen@gmail.com>
2024-09-24 20:19:55 -0700

    count characters added

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = count_words(text)
+    word_count = count_words(text)
+    char_count_dict = count_chars(text)
     print(text)
-    print(count)
-
+    print(f"There are {word_count} words in this book.")
+    print(char_count_dict)
 
 def get_book_text(path):    
     with open(path) as f:
         return f.read()
-    
-def count_words(string):
-    words = string.split()
+
+def count_chars(text):
+    char_dict = {}
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1 
+    return char_dict
+
+def count_words(text):
+    words = text.split()
     count = len(words)
     return count
 
