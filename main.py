ae68c9aa148bd508e026bec9e3cbbd6dfdd5575d
mattersen <mattersen@gmail.com>
2024-09-26 12:47:11 -0700

    added report function

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     char_count_dict = count_chars(text)
-    print(text)
-    print(f"There are {word_count} words in this book.")
-    print(char_count_dict)
+    unique_chars_list = dict_to_list(char_count_dict)
+    unique_chars_list.sort(reverse=True, key=sort_on)
+    alpha_only_dict = alpha_dict(unique_chars_list)
+
+    print_report(alpha_only_dict, book_path, word_count)
 
 def get_book_text(path):    
     with open(path) as f:
@@ -24,5 +26,29 @@ def count_words(text):
     words = text.split()
     count = len(words)
     return count
+
+def dict_to_list(dict):
+    list = []
+    for char in dict:
+        list.append({"char" : char, "num": dict[char]})
+    return list
+
+def alpha_dict(list_of_dict):
+    alpha_dict = {}
+    for dict in list_of_dict:
+        if dict["char"].isalpha():
+            alpha_dict[dict["char"]] = dict["num"]
+    return alpha_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(alpha_sorted_dictionary: dict[str, int], book_path: str, word_count: int):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"There are {word_count} words in this book.")
+    print("")
+    for char, num in alpha_sorted_dictionary.items():
+        print(f"The '{char}' character was found {num} times")
+    print("--- End Report ---")
 
 main()
